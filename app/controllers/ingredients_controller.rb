323b316be535fc99c05084e4af25495a0529f38f
James Devereux <devereuxjj@gmail.com>
2019-02-21 17:32:23 +0900

    done

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,13 @@
+class IngredientsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+private
+
+  def ingredient_params
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts "Calling API to get ingredients.."
+
+url_serialized = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+json = JSON.parse(url_serialized)
+
+puts "Making ingredient instances...."
+
+json['drinks'].each_with_index do | _item, index |
+  Ingredient.create(
+    name: json['drinks'][index]['strIngredient1']
+    )
+end
+
+puts "Finished!"
